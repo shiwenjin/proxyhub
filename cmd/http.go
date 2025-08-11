9963184f97e204701e36c05f5c134b4e1374c7cd
Rick <zh376981735@gmail.com>
2025-08-12 01:17:23 +0800

    refactor(cmd): 重构HTTP和SOCKS代理的流量统计代码

--- cmd/http.go
+++ cmd/http.go
@@ -17,103 +17,107 @@ import (
 
 var httpArgs services.HTTPArgs
 
+// setupTrafficReporter 初始化流量报告器
+func setupTrafficReporter() (*services.TrafficReporter, chan services.TrafficRecord) {
+	reporter := services.NewTrafficReporter(&args, "http")
+	var recordsCh chan services.TrafficRecord
+
+	if reporter != nil && reporterModeFast(reporter) && reporterFastGlobal(reporter) {
+		recordsCh = make(chan services.TrafficRecord, 2048)
+		reporter.StartGlobalBatch(recordsCh)
+	}
+
+	return reporter, recordsCh
+}
+
+// createDialHandler 创建处理普通HTTP请求的拨号处理器
+func createDialHandler(reporter *services.TrafficReporter, recordsCh chan services.TrafficRecord) func(ctx context.Context, network, addr string) (net.Conn, error) {
+	return func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return wrapConnection(ctx, network, addr, reporter, recordsCh)
+	}
+}
+
+// createConnectDialHandler 创建处理HTTPS CONNECT请求的拨号处理器
+func createConnectDialHandler(reporter *services.TrafficReporter, recordsCh chan services.TrafficRecord) func(network, addr string) (net.Conn, error) {
+	return func(network, addr string) (net.Conn, error) {
+		return wrapConnection(context.Background(), network, addr, reporter, recordsCh)
+	}
+}
+
+// wrapConnection 包装连接以进行流量统计
+func wrapConnection(ctx context.Context, network, addr string, reporter *services.TrafficReporter, recordsCh chan services.TrafficRecord) (net.Conn, error) {
+	outConn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	if reporter == nil {
+		return outConn, nil
+	}
+
+	c := &services.CountingConn{Conn: outConn}
+	serverAddr := *args.Local
+	clientAddr := "" // 在这里获取客户端地址不太容易
+	targetAddr := addr
+
+	if !reporterModeFast(reporter) {
+		c.SetOnClose(func(total int64) {
+			rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", total)
+			_ = reporter.ReportOnce(rec)
+		})
+		return c, nil
+	}
+
+	interval := reporterInterval(reporter)
+	if reporterFastGlobal(reporter) && recordsCh != nil {
+		go perConnDeltaToChan(ctx, c, interval, func(d int64) {
+			if d > 0 {
+				recordsCh <- reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
+			}
+		})
+	} else {
+		go perConnDeltaToReport(ctx, c, interval, func(d int64) {
+			if d > 0 {
+				_ = reporter.ReportOnce(reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d))
+			}
+		})
+	}
+
+	return c, nil
+}
+
+// startHTTPServer 启动HTTP代理服务器
+func startHTTPServer(proxy *goproxy.ProxyHttpServer) {
+	log.Info("http ", zap.String("port", *args.Local))
+
+	err := http.ListenAndServe(*args.Local, proxy)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
 // httpCmd represents the http command
 var httpCmd = &cobra.Command{
 	Use:              "http",
 	TraverseChildren: true,
 	Run: func(cmd *cobra.Command, _ []string) {
+		// 创建代理服务器
 		proxy := goproxy.NewProxyHttpServer()
 		proxy.Verbose = *args.Verbose
 
-		// Traffic reporter (optional)
-		reporter := services.NewTrafficReporter(&args, "http")
-		var recordsCh chan services.TrafficRecord
-		if reporter != nil && reporterModeFast(reporter) && reporterFastGlobal(reporter) {
-			recordsCh = make(chan services.TrafficRecord, 2048)
-			reporter.StartGlobalBatch(recordsCh)
-		}
+		// 设置流量报告器
+		reporter, recordsCh := setupTrafficReporter()
 
-		// Custom transport to wrap dials
+		// 设置HTTP传输
 		tr := &http.Transport{}
-		tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-			outConn, err := net.Dial(network, addr)
-			if err != nil {
-				return nil, err
-			}
-			if reporter == nil {
-				return outConn, nil
-			}
-			c := &services.CountingConn{Conn: outConn}
-			serverAddr := *args.Local
-			clientAddr := "" // not trivial to fetch here
-			targetAddr := addr
-			if !reporterModeFast(reporter) {
-				c.SetOnClose(func(total int64) {
-					rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", total)
-					_ = reporter.ReportOnce(rec)
-				})
-				return c, nil
-			}
-			interval := reporterInterval(reporter)
-			if reporterFastGlobal(reporter) && recordsCh != nil {
-				go perConnDeltaToChan(ctx, c, interval, func(d int64) {
-					if d > 0 {
-						recordsCh <- reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
-					}
-				})
-			} else {
-				go perConnDeltaToReport(ctx, c, interval, func(d int64) {
-					if d > 0 {
-						_ = reporter.ReportOnce(reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d))
-					}
-				})
-			}
-			return c, nil
-		}
+		tr.DialContext = createDialHandler(reporter, recordsCh)
 		proxy.Tr = tr
-		proxy.ConnectDial = func(network, addr string) (net.Conn, error) {
-			// HTTPS CONNECT
-			outConn, err := net.Dial(network, addr)
-			if err != nil {
-				return nil, err
-			}
-			if reporter == nil {
-				return outConn, nil
-			}
-			c := &services.CountingConn{Conn: outConn}
-			serverAddr := *args.Local
-			clientAddr := ""
-			targetAddr := addr
-			if !reporterModeFast(reporter) {
-				c.SetOnClose(func(total int64) {
-					rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", total)
-					_ = reporter.ReportOnce(rec)
-				})
-				return c, nil
-			}
-			interval := reporterInterval(reporter)
-			if reporterFastGlobal(reporter) && recordsCh != nil {
-				go perConnDeltaToChan(context.Background(), c, interval, func(d int64) {
-					if d > 0 {
-						recordsCh <- reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
-					}
-				})
-			} else {
-				go perConnDeltaToReport(context.Background(), c, interval, func(d int64) {
-					if d > 0 {
-						_ = reporter.ReportOnce(reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d))
-					}
-				})
-			}
-			return c, nil
-		}
 
-		log.Info("http ", zap.String("port", *args.Local))
+		// 设置HTTPS CONNECT处理
+		proxy.ConnectDial = createConnectDialHandler(reporter, recordsCh)
 
-		err := http.ListenAndServe(*args.Local, proxy)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		// 启动服务器
+		startHTTPServer(proxy)
 	},
 }
 
--- cmd/socks.go
+++ cmd/socks.go
@@ -12,99 +12,127 @@ import (
     "go.uber.org/zap"
 )
 
+// setupSocksTrafficReporter 初始化SOCKS流量报告器
+func setupSocksTrafficReporter() (*services.TrafficReporter, chan services.TrafficRecord) {
+    reporter := services.NewTrafficReporter(&args, "socks")
+    var recordsCh chan services.TrafficRecord
+
+    if reporter != nil && reporterModeFast(reporter) && reporterFastGlobal(reporter) {
+        recordsCh = make(chan services.TrafficRecord, 1024)
+        reporter.StartGlobalBatch(recordsCh)
+    }
+
+    return reporter, recordsCh
+}
+
+// handleSocksConnection 处理SOCKS连接并进行流量统计
+func handleSocksConnection(ctx context.Context, network, addr string, request *socks5.Request, 
+                           reporter *services.TrafficReporter, recordsCh chan services.TrafficRecord) (net.Conn, error) {
+    log.Info("socks request", zap.Any("remote", request.RemoteAddr), zap.Any("dest", request.DestAddr))
+    
+    // 连接真实服务器
+    outConn, err := net.Dial(network, addr)
+    if err != nil { 
+        return nil, err 
+    }
+
+    if reporter == nil {
+        return outConn, nil
+    }
+
+    // 包装连接以进行计数
+    c := &services.CountingConn{Conn: outConn}
+    serverAddr := *args.Local
+    clientAddr := ""
+    if request.RemoteAddr != nil { 
+        clientAddr = request.RemoteAddr.String() 
+    }
+    targetAddr := ""
+    if request.DestAddr != nil { 
+        targetAddr = request.DestAddr.String() 
+    }
+
+    // 普通模式：在连接关闭时报告总字节数
+    if !reporterModeFast(reporter) {
+        c.SetOnClose(func(total int64) {
+            rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", total)
+            _ = reporter.ReportOnce(rec)
+        })
+        return c, nil
+    }
+
+    // 快速模式（全局或每连接）
+    interval := reporterInterval(reporter)
+    if reporterFastGlobal(reporter) && recordsCh != nil {
+        // 发送增量到全局通道
+        go monitorConnectionTraffic(ctx, c, interval, func(d int64) {
+            if d > 0 {
+                rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
+                recordsCh <- rec
+            }
+        })
+    } else {
+        // 每连接快速模式：直接GET请求增量
+        go monitorConnectionTraffic(ctx, c, interval, func(d int64) {
+            if d > 0 {
+                rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
+                _ = reporter.ReportOnce(rec)
+            }
+        })
+    }
+
+    return c, nil
+}
+
+// monitorConnectionTraffic 监控连接流量并通过回调函数报告
+func monitorConnectionTraffic(ctx context.Context, c *services.CountingConn, interval time.Duration, report func(int64)) {
+    ticker := time.NewTicker(interval)
+    defer ticker.Stop()
+    for {
+        select {
+        case <-ctx.Done():
+            // 计划A中没有最终报告
+            return
+        case <-ticker.C:
+            d := c.Delta()
+            if d > 0 {
+                report(d)
+            }
+        }
+    }
+}
+
+// createSocksServer 创建并配置SOCKS5服务器
+func createSocksServer(reporter *services.TrafficReporter, recordsCh chan services.TrafficRecord) *socks5.Server {
+    return socks5.NewServer(
+        socks5.WithLogger(log.Default.Sugar()),
+        socks5.WithDialAndRequest(func(ctx context.Context, network, addr string, request *socks5.Request) (net.Conn, error) {
+            return handleSocksConnection(ctx, network, addr, request, reporter, recordsCh)
+        }),
+    )
+}
+
+// startSocksServer 启动SOCKS5服务器
+func startSocksServer(server *socks5.Server) {
+    log.Info("SOCKS5 server started", zap.String("addr", *args.Local))
+    if err := server.ListenAndServe("tcp", *args.Local); err != nil {
+        panic(err)
+    }
+}
+
 // socksCmd represents the socks command
 var socksCmd = &cobra.Command{
     Use:   "socks",
     Short: "SOCKS 代理",
     Run: func(cmd *cobra.Command, _ []string) {
-        // Traffic reporter (optional)
-        reporter := services.NewTrafficReporter(&args, "socks")
-        var recordsCh chan services.TrafficRecord
-        if reporter != nil && reporterModeFast(reporter) && reporterFastGlobal(reporter) {
-            recordsCh = make(chan services.TrafficRecord, 1024)
-            reporter.StartGlobalBatch(recordsCh)
-        }
+        // 设置流量报告器
+        reporter, recordsCh := setupSocksTrafficReporter()
 
-        // Create a SOCKS5 server
-        server := socks5.NewServer(
-            socks5.WithLogger(log.Default.Sugar()),
-            socks5.WithDialAndRequest(func(ctx context.Context, network, addr string, request *socks5.Request) (net.Conn, error) {
-                log.Info("socks request", zap.Any("remote", request.RemoteAddr), zap.Any("dest", request.DestAddr))
-                // dial real server
-                outConn, err := net.Dial(network, addr)
-                if err != nil { return nil, err }
+        // 创建SOCKS5服务器
+        server := createSocksServer(reporter, recordsCh)
 
-                if reporter == nil {
-                    return outConn, nil
-                }
-
-                // wrap counting
-                c := &services.CountingConn{Conn: outConn}
-                serverAddr := *args.Local
-                clientAddr := ""
-                if request.RemoteAddr != nil { clientAddr = request.RemoteAddr.String() }
-                targetAddr := ""
-                if request.DestAddr != nil { targetAddr = request.DestAddr.String() }
-
-                // normal: report on close total bytes
-                if !reporterModeFast(reporter) {
-                    c.SetOnClose(func(total int64) {
-                        rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", total)
-                        _ = reporter.ReportOnce(rec)
-                    })
-                    return c, nil
-                }
-
-                // fast (per-conn or global)
-                interval := reporterInterval(reporter)
-                if reporterFastGlobal(reporter) && recordsCh != nil {
-                    // send deltas to global channel
-                    go func() {
-                        ticker := time.NewTicker(interval)
-                        defer ticker.Stop()
-                        for {
-                            select {
-                            case <-ctx.Done():
-                                // no final report in plan A
-                                return
-                            case <-ticker.C:
-                                d := c.Delta()
-                                if d > 0 {
-                                    rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
-                                    recordsCh <- rec
-                                }
-                            }
-                        }
-                    }()
-                } else {
-                    // per-connection fast: direct GET with delta
-                    go func() {
-                        ticker := time.NewTicker(interval)
-                        defer ticker.Stop()
-                        for {
-                            select {
-                            case <-ctx.Done():
-                                // no final report in plan A
-                                return
-                            case <-ticker.C:
-                                d := c.Delta()
-                                if d > 0 {
-                                    rec := reporter.BuildRecord(serverAddr, clientAddr, targetAddr, outConn, "", "", d)
-                                    _ = reporter.ReportOnce(rec)
-                                }
-                            }
-                        }
-                    }()
-                }
-
-                return c, nil
-            }),
-        )
-
-        log.Info("SOCKS5 server started", zap.String("addr", *args.Local))
-        if err := server.ListenAndServe("tcp", *args.Local); err != nil {
-            panic(err)
-        }
+        // 启动服务器
+        startSocksServer(server)
     },
 }
 
